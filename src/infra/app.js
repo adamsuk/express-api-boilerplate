873eb7d4f6053982123bdabd47c575d79a3553ce
Scott <sra405@hotmail.co.uk>
2021-06-23 21:46:24 +0100

    ci(vulnerability ci): added initial Snyk scanning

--- src/infra/app.js
+++ src/infra/app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 const R = require('ramda');
+var sanitizer = require('sanitizer');
 
 var routes = require("./../routes");
 var schema = require("./schema");
@@ -12,15 +13,16 @@ app.use(express.json())
 var router = express.Router()
 
 router.use(function async (req, res, next) {
+    var req_body = sanitizer.sanitize(req.body);
     // initial logging (can be moved to logger)
     console.log(`Incoming request method ${req.method} on path ${req.url}`);
-    if (Object.keys(req.body).length !== 0) {
-        console.log(req.body);
+    if (Object.keys(req_body).length !== 0) {
+        console.log(req_body);
     };
 
     if (R.has(req.method)(schema)) {
         // validate the request based on incoming method
-        var valid_req = schema[req.method].validate(req.body);
+        var valid_req = schema[req.method].validate(req_body);
         if (valid_req.error !== undefined) {
             console.log(`Request validation: ${valid_req.error}`);
             res.status(500).send(valid_req.error);
